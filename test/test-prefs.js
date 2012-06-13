80371fd66b4b5cca105f3ebc83ce3717427f3019
Leo Ekleog Gaspard <ekleog@gmail.com>
2012-06-13 21:11:28 +0200

    Implemented a prefs.js module

---
+++ test/test-prefs.js
@@ -0,0 +1,46 @@
+const prefs = require("prefs");
+
+exports.test_set = function(test) {
+   prefs.set("set", "burk");
+   test.assertEqual(require("preferences-service")
+                     .get("extensions.mailock.set"),
+                    "burk");
+}
+
+exports.test_get = function(test) {
+   prefs.set("get", "bark");
+   test.assertEqual(prefs.get("get"), "bark");
+   test.assertEqual(prefs.get("get_invalid", 42), 42);
+}
+
+exports.test_has = function(test) {
+   test.assert(!prefs.has("has"));
+   test.assert(!prefs.has("has_not"));
+   prefs.set("has", "berk");
+   test.assert(prefs.has("has"));
+   test.assert(!prefs.has("has_not"));
+}
+
+exports.test_isSet = function(test) {
+   test.assert(!prefs.isSet("isset"));
+   prefs.set("isset", "abc");
+   test.assert(prefs.isSet("isset"));
+}
+
+exports.test_reset = function(test) {
+   test.assertUndefined(prefs.get("reset"));
+   prefs.set("reset", "42");
+   test.assertEqual(prefs.get("reset"), "42");
+   prefs.reset("reset");
+   test.assertUndefined(prefs.get("reset"));
+}
+
+exports.test_getLocalized = function(test) {
+   // TODO: Fill in, understand
+   test.pass();
+}
+
+exports.test_setLocalized = function(test) {
+   // TODO: Fill in, understand
+   test.pass();
+}
