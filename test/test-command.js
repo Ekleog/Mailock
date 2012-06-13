775b3ab30cdd5fed487e12396356abc50e309264
Leo Ekleog Gaspard <ekleog@gmail.com>
2012-06-13 17:15:22 +0200

    Fixed compatibility of testing with non-linux-like systems quite brutally : they just can't

--- test/test-command.js
+++ test/test-command.js
@@ -5,32 +5,44 @@ function randomString() {
    // Gracefully taken at http://stackoverflow.com/questions/1349404
    var text = "";
    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-   for(var i = 0; i < 5; i++) {
+   for (var i = 0; i < 5; i++) {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
    }
    return text;
 }
 
 function mktemp(base) {
-   if (file.exists("/tmp"))
-      return "/tmp/"     + base + "-" + randomString();
-   else
-      return "C:\\tmp\\" + base + "-" + randomString();
+   return "/tmp/" + base + "-" + randomString();
 }
 
 function touch(filename) {
-   // TODO: Build platform-independant test
    return "/usr/bin/touch " + filename;
 }
 
-exports.test_run = function(test) {
+function check(test) {
+   if (file.exists("/tmp/") && file.exists("/usr/bin/touch")) {
+      return true;
+   } else {
+      test.fail(
+         "Can test command.js only on linux-like systems " +
+         "(requires a /tmp directory and a /usr/bin/touch utility)"
+      );
+      return false;
+   }
+}
+
+function checked(func) {
+   return function(test) { if (check(test)) func(test); }
+}
+
+exports.test_run = checked(function(test) {
    var filename = mktemp("test-command-run");
    command.run(touch(filename));
    test.assert(file.exists(filename), "Command not being executed");
    file.remove(filename);
-}
+})
 
-exports.test_runAsync = function(test) {
+exports.test_runAsync = checked(function(test) {
    var filename = mktemp("test-command-runAsync");
    command.runAsync(touch(filename), function() {
       test.assert(file.exists(filename),
@@ -39,4 +51,4 @@ exports.test_runAsync = function(test) {
       test.done();
    });
    test.waitUntilDone(1000);
-}
+})
