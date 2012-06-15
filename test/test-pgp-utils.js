8c24e64511a44be3e8879c8a358367933efdc8e3
Leo Ekleog Gaspard <ekleog@gmail.com>
2012-06-15 15:55:19 +0200

    Added pgp-utils.js test ; fixed it

--- test/test-pgp-utils.js
+++ test/test-pgp-utils.js
@@ -1,1 +1,7 @@
-// TODO
+const pgp_utils = require("pgp-utils");
+
+exports.test_keyid = function(test) {
+   test.assertEqual(pgp_utils.keyid("75DC9CAAAFFB3F629E315F5936621BE556D9F845"),
+                     "56D9F845",
+                     "Wrong extraction of key ID from fingerprint");
+}
