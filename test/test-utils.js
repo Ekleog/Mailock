04b9b94b3a77658f0e928f553db4da8f0b0355ee
Leo Ekleog Gaspard <ekleog@gmail.com>
2012-06-15 15:50:21 +0200

    Added utils.js test

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,1 +1,13 @@
-// TODO
+const utils = require("utils");
+
+exports.test_unique = function(test) {
+   test.assertEqual(utils.unique(["a", "a", "b", "c", "c", "d"]).toString(),
+                     ["a", "b", "c", "d"].toString(),
+                     "Not removing duplicates");
+   test.assertEqual(utils.unique(["a", "b", "c", "d"]).toString(),
+                     ["a", "b", "c", "d"].toString(),
+                     "Removing non duplicates");
+   test.assertEqual(utils.unique([42, "b", 42, "c"]).toString(),
+                     [42, "b", "c"].toString(),
+                     "Not removing separated duplicates");
+}
